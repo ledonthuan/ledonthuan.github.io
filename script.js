24677f4b1430c6d27474ccae3313f78bb9ced06a
Don-Thuan Le <ledonthuan@gmail.com>
2024-07-23 21:14:36 -0400

    first scene test

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
 async function init() {
-    const data = await d3.csv("https://flunky.github.io/cars2017.csv");
-    console.log(data);
+    // Load the dataset
+    const data = await d3.csv("owid-co2-data.csv", d3.autoType);
+    console.log(data);  // Verify data is loaded correctly
 
-    var svg = d3.select("svg");
-    var g = svg.append("g").attr("transform", "translate(50,50)");
+    // Filter data for a specific country (e.g., 'World')
+    const worldData = data.filter(d => d.country === 'World');
 
-    var x = d3.scaleLog()
-        .base(10)
-        .domain([10, 150])
-        .range([0, 200]);
+    // Initialize SVG
+    const svg = d3.select("svg");
+    const width = +svg.attr("width");
+    const height = +svg.attr("height");
+    const margin = {top: 20, right: 30, bottom: 40, left: 50};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-    var y = d3.scaleLog()
-        .base(10)
-        .domain([10, 150])
-        .range([200, 0]);
+    // Set up scales
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(worldData, d => d.year))
+        .range([0, innerWidth]);
 
-    g.selectAll("circle")
-        .data(data)
-        .enter().append("circle")
-        .attr("cx", function(d) { return x(d.AverageCityMPG); })
-        .attr("cy", function(d) { return y(d.AverageHighwayMPG); })
-        .attr("r", function(d, i) { return 2 + +d.EngineCylinders; });
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(worldData, d => d.co2)])
+        .range([innerHeight, 0]);
 
-    var xAxis = d3.axisBottom(x)
-        .tickValues([10, 20, 50, 100])
-        .tickFormat(d3.format("~s"));
+    // Set up axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
 
-    var yAxis = d3.axisLeft(y)
-        .tickValues([10, 20, 50, 100])
-        .tickFormat(d3.format("~s"));
+    // Append axes
+    g.append("g").call(yAxis);
+    g.append("g").call(xAxis)
+        .attr("transform", `translate(0,${innerHeight})`);
 
-    svg.append("g")
-        .attr("transform", "translate(50,50)")
-        .call(yAxis);
+    // Create the line generator
+    const line = d3.line()
+        .x(d => xScale(d.year))
+        .y(d => yScale(d.co2));
 
-    svg.append("g")
-        .attr("transform", "translate(50,250)")
-        .call(xAxis);
+    // Draw the line
+    g.append("path")
+        .datum(worldData)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 }
 
 init();
