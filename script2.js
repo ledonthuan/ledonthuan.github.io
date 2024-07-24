0f2633e3ad1fa9d5f07c5a6943cc598445c7095f
Don-Thuan Le <ledonthuan@gmail.com>
2024-07-23 21:32:06 -0400

    first scene test

--- script2.js
+++ script2.js
@@ -7,8 +7,30 @@ async function init() {
     const svg = d3.select("svg");
     const g = svg.append("g").attr("transform","translate(50,50)");
 
+    // only get the world data
+    const worldData = data.filter(d => d.country === 'World');
+
     //set up scales
-    const xScale = d3.scaleTime
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(worldData, d => d.year))
+        .range([0, 500]);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(worldData, d => d.co2)])
+        .range([300, 0]);
+
+    // Create the line generator
+    const line = d3.line()
+        .x(d => xScale(d.year))
+        .y(d => yScale(d.co2));
+
+    // Draw the line
+    g.append("path")
+        .datum(worldData)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 
     // Add annotations, interactivity, etc. here
 }
