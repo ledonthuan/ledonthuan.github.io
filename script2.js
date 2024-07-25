341235a7e4c945bb2527d828a0beec47e7b2c6ec
Don-Thuan Le <ledonthuan@gmail.com>
2024-07-24 21:12:06 -0400

    update

--- script2.js
+++ script2.js
@@ -5,7 +5,7 @@ async function init() {
     // Parse the data
     data.forEach(d => {
         d.year = +d.year;  // Parse year as an integer
-        d.co2 = +d.co2 * 1000000;  // Ensure co2 is a number
+        d.co2 = +d.co2;  // Ensure co2 is a number
     });
 
     // Filter for the world data
@@ -26,13 +26,17 @@ async function init() {
     const xScale = d3.scaleTime()
         .range([0, innerWidth]);
 
+    // Set up y scale and axis format to display in millions of tons
     const yScale = d3.scaleLinear()
         .range([innerHeight, 0]);
+
+    // Create a custom format function for y-axis
+    const formatYAxis = d3.format(".2s");  // For large numbers (like 35,000,000), this can show "35M"
 
     // Set up axes
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale)
-        .tickFormat(d3.format(".2s"));  // Format y-axis ticks
+        .tickFormat(d => formatYAxis(d / 1e6) + 'M');  // Convert to millions of tons
 
     // Append axes
     const xAxisGroup = g.append("g")
@@ -78,80 +82,3 @@ async function init() {
 }
 
 init();
-
-
-// async function init() {
-//     // Load the dataset
-//     const data = await d3.csv("owid-co2-data.csv");
-
-//     // Parse the data
-//     data.forEach(d => {
-//         d.year = +d.year;  // Parse year as an integer
-//         d.co2 = +d.co2;
-//     });
-
-//     // Filter for the world data
-//     const worldData = data.filter(d => d.country === 'World');
-
-//     // Initialize SVG dimensions
-//     const svg = d3.select("svg");
-//     const width = +svg.attr("width") || 600;
-//     const height = +svg.attr("height") || 400;
-//     const margin = { top: 20, right: 30, bottom: 40, left: 70 };
-//     const innerWidth = width - margin.left - margin.right;
-//     const innerHeight = height - margin.top - margin.bottom;
-
-//     const g = svg.append("g")
-//         .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-//     // Set up scales
-//     const xScale = d3.scaleTime()
-//         .range([0, innerWidth]);
-
-//     const yScale = d3.scaleLinear()
-//         .range([innerHeight, 0]);
-
-//     // Set up axes
-//     const xAxis = d3.axisBottom(xScale);
-//     const yAxis = d3.axisLeft(yScale)
-//         .tickFormat(d3.format(".2s"));
-
-//     // Append axes
-//     const xAxisGroup = g.append("g")
-//         .attr("transform", `translate(0,${innerHeight})`);
-//     const yAxisGroup = g.append("g");
-
-//     // Create the line generator
-//     const line = d3.line()
-//         .x(d => xScale(new Date(d.year, 0, 1)))  // Use only the year for the x value
-//         .y(d => yScale(d.co2));
-
-//     const path = g.append("path")
-//         .attr("fill", "none")
-//         .attr("stroke", "steelblue")
-//         .attr("stroke-width", 1.5);
-
-//     function updateChart() {
-//         const startYear = +document.getElementById("startDate").value;
-//         const endYear = +document.getElementById("endDate").value;
-
-//         const filteredData = worldData.filter(d => d.year >= startYear && d.year <= endYear);
-
-//         xScale.domain(d3.extent(filteredData, d => new Date(d.year, 0, 1)));
-//         yScale.domain([0, d3.max(filteredData, d => d.co2)]);
-
-//         xAxisGroup.call(xAxis);
-//         yAxisGroup.call(yAxis);
-
-//         path.datum(filteredData)
-//             .attr("d", line);
-//     }
-
-//     document.getElementById("updateButton").addEventListener("click", updateChart);
-
-//     // Initial chart render
-//     updateChart();
-// }
-
-// init();
-
