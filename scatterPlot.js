af19d1e3b3917a49091c54eb35dac9cc7d823cd4
Don Le <don@Serenas-MacBook-Pro.local>
2024-07-26 15:13:05 -0400

    update

--- scatterPlot.js
+++ scatterPlot.js
@@ -12,7 +12,7 @@ async function initScatterPlot() {
     const yScale = d3.scaleLinear().range([innerHeight, 0]);
 
     const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
+    const yAxis = d3.axisLeft(yScale).tickFormat(d3.format(".2s"));
 
     const xAxisGroup = g.append("g").attr("transform", `translate(0,${innerHeight})`);
     const yAxisGroup = g.append("g");
@@ -20,36 +20,35 @@ async function initScatterPlot() {
     const data = await d3.csv("owid-co2-data.csv", d => ({
         year: +d.year,
         co2: +d.co2,
+        gdp: +d.gdp,
         country: d.country,
-        gdp: +d.gdp
+        iso_code: d.iso_code
     }));
 
     function updateScatterPlot() {
         const year = +document.getElementById("year").value;
-        const filteredData = data.filter(d => d.year === year);
+        const yearData = data.filter(d => d.year === year && d.iso_code);
 
-        xScale.domain(d3.extent(filteredData, d => d.gdp));
-        yScale.domain(d3.extent(filteredData, d => d.co2));
+        xScale.domain([0, d3.max(yearData, d => d.gdp)]);
+        yScale.domain([0, d3.max(yearData, d => d.co2)]);
 
         xAxisGroup.call(xAxis);
         yAxisGroup.call(yAxis);
 
-        const circles = g.selectAll(".circle")
-            .data(filteredData, d => d.country);
+        const circles = g.selectAll("circle").data(yearData);
 
         circles.enter().append("circle")
-            .attr("class", "circle")
             .attr("cx", d => xScale(d.gdp))
             .attr("cy", d => yScale(d.co2))
             .attr("r", 5)
             .attr("fill", "steelblue");
 
-        circles.exit().remove();
-
-        circles.transition()
-            .attr("cx", d => xScale(d.gdp))
+        circles.attr("cx", d => xScale(d.gdp))
             .attr("cy", d => yScale(d.co2))
-            .attr("r", 5);
+            .attr("r", 5)
+            .attr("fill", "steelblue");
+
+        circles.exit().remove();
     }
 
     document.getElementById("scatterPlot").addEventListener('update', updateScatterPlot);
